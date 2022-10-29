89b9160bcfe3cb8e2e8c27a307c508887645b7b6
syo-sa1982 <fujii1308@gmail.com>
2022-10-30 00:50:40 +0900

    refact:後続の変更を見据えてUIの構造を変更

--- app/src/main/java/dev/syousa1982/hiittimer/MainActivity.kt
+++ app/src/main/java/dev/syousa1982/hiittimer/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.syousa1982.hiittimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import dev.syousa1982.hiittimer.ui.theme.HIITTimerTheme
@@ -24,12 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HIITTimerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    TimerApp(
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
+                TimerApp()
             }
         }
     }
@@ -39,18 +37,22 @@ class MainActivity : ComponentActivity() {
 fun TimerApp(
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("終了")
-        Button(
-            onClick = { /* Do something */ },
+    Surface(color = MaterialTheme.colors.background) {
+
+//        Image(bitmap = ImageBitmap.imageResource(), contentDescription = null)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("スタート")
+            Text("終了")
+            Button(
+                onClick = { /* Do something */ },
+            ) {
+                Text("スタート")
+            }
+            Text("10")
         }
-        Text("10")
     }
 //    val secs = countTimeViewModel.seconds.observeAsState()
 //    val minutes = countTimeViewModel.minutes.observeAsState()
