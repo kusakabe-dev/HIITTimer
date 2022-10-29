93d1f9e3e2a488ba9056813ca44e8ca110f95e78
syo-sa1982 <fujii1308@gmail.com>
2022-10-29 13:08:14 +0900

    一旦、UIだけ雑に作成

--- app/src/main/java/dev/syousa1982/hiittimer/MainActivity.kt
+++ app/src/main/java/dev/syousa1982/hiittimer/MainActivity.kt
@@ -3,10 +3,16 @@ package dev.syousa1982.hiittimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,7 +26,9 @@ class MainActivity : ComponentActivity() {
             HIITTimerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-//                    TimerApp()
+                    TimerApp(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
@@ -28,12 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TimerApp(countTimeViewModel: CountTimeViewModel, modifier: Modifier = Modifier) {
-    val secs = countTimeViewModel.seconds.observeAsState()
-    val minutes = countTimeViewModel.minutes.observeAsState()
-    val hours = countTimeViewModel.hours.observeAsState()
-
-    val resumed = countTimeViewModel.isRunning.observeAsState()
+fun TimerApp(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("終了")
+        Button(
+            onClick = { /* Do something */ },
+        ) {
+            Text("スタート")
+        }
+        Text("10")
+    }
+//    val secs = countTimeViewModel.seconds.observeAsState()
+//    val minutes = countTimeViewModel.minutes.observeAsState()
+//    val hours = countTimeViewModel.hours.observeAsState()
+//
+//    val resumed = countTimeViewModel.isRunning.observeAsState()
 //    val progress = countTimeViewModel.progress.observeAsState(1f)
 //    val timeShow = countTimeViewModel.time.observeAsState(initial = "00:00:00")
 }
@@ -43,7 +66,9 @@ fun TimerApp(countTimeViewModel: CountTimeViewModel, modifier: Modifier = Modifi
 fun DefaultPreview() {
     HIITTimerTheme {
         Surface(color = MaterialTheme.colors.background) {
-//            TimerApp()
+            TimerApp(
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
